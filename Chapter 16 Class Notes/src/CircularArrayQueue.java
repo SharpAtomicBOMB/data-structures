e3e13d051da7c291ff36e12fa69cbdab5be00062
SharpAtomicBOMB <dkozlowski@stu.naperville203.org>
2024-10-01 14:18:22 -0500

    notes

--- Chapter 16 Class Notes/src/CircularArrayQueue.java
+++ Chapter 16 Class Notes/src/CircularArrayQueue.java
@@ -1,4 +1,6 @@
+
 import java.util.NoSuchElementException;
+
 
 /**
     An implementation of a queue as a circular array.
@@ -6,14 +8,25 @@ import java.util.NoSuchElementException;
 public class CircularArrayQueue
 {
     private Object[] elements;
-    //private data
+    private int head;
+    private int tail;
+    private int currentSize;
 
+    
+    //private data
+    
 
 
     /**
         Constructs an empty queue.
     */
-
+    public CircularArrayQueue(){
+        final int INITIAL_SIZE = 5;
+        this.elements = new Object[INITIAL_SIZE];
+        head = 0;
+        tail = 0;
+        currentSize = 0; // the queue is empty when we first create it
+    }
 
 
 
@@ -23,15 +36,22 @@ public class CircularArrayQueue
         Checks whether this queue is empty.
         @return true if this queue is empty
     */
-
-
-
+    public boolean empty(){
+        return currentSize == 0;
+    }
 
     /**
         Adds an element to the tail of this queue.
         @param newElement the element to add
     */
-
+    public void add(Object element){
+        this.currentSize++;
+        this.elements[this.tail] = element;
+        this.tail++;
+        //reset tail to 0 when it reachers the size of the array with modulous
+        this.tail %= this.elements.length; 
+        growIfNecessary();
+    }
 
 
 
@@ -40,7 +60,16 @@ public class CircularArrayQueue
         Removes an element from the head of this queue.
         @return the removed element
     */
-
+    public Object remove(){
+        if(this.empty()){
+            throw new NoSuchElementException();
+        }
+        this.currentSize--;
+        Object element = this.elements[this.head];
+        // reset the head to 0 if we reach the end of the arrray
+        this.head = (this.head + 1)% this.elements.length;
+        return element;
+    }
 
 
 
@@ -50,7 +79,7 @@ public class CircularArrayQueue
     */
     private void growIfNecessary()
     {
-        /*
+        
         if(this.currentSize == this.elements.length)
         {
             Object[] newElements = new Object[2 * this.elements.length];
@@ -62,7 +91,7 @@ public class CircularArrayQueue
             this.head = 0;
             this.tail = this.currentSize;
         }
-        */
+    
     }
 
 
--- Chapter 16 Class Notes/src/LinkedListStack.java
+++ Chapter 16 Class Notes/src/LinkedListStack.java
@@ -20,7 +20,14 @@ public class LinkedListStack
      *
      * @param element the element to add
     */
+    public void push(Object element){
+        //Always push elements to the front of the linked list
+        Node newNode = new Node();
+        newNode.data = element;
+        newNode.next = this.first;
+        this.first = newNode;
 
+    }
 
 
 
@@ -30,7 +37,20 @@ public class LinkedListStack
         Removes the element from the top of the stack.
         @return the removed element
     */
+    public Object pop(){
+        
+        if(this.empty()){
+            throw new NoSuchElementException();
+        }
+        
+        //check if theres something in the list
+        Object element = this.first.data;
+        
+        
+        this.first = this.first.next;
+        return element;
 
+    }
 
 
 
@@ -43,7 +63,9 @@ public class LinkedListStack
      *
      * @return true if the stack is empty
     */
-
+    public boolean empty(){
+        return this.first == null;
+    }
 
     static class Node
     {
