ddde53e0b554875881d5d0cd81850a68d0f946f7
SharpAtomicBOMB <dkozlowski@stu.naperville203.org>
2024-10-03 22:09:43 -0500

    added some work to linkedlist

--- Chapter 16 Activities/RecursiveSize/src/LinkedList.java
+++ Chapter 16 Activities/RecursiveSize/src/LinkedList.java
@@ -24,9 +24,14 @@ public class LinkedList
         Computes the size of the linked list.
         @return the number of elements in the list
     */
-    public int size()
+    public int size(Node start)
     {
-        . . .
+        if(start == null){
+            return 0;
+        }
+        else{
+            return(1+size(start.next));
+        }
     }
 
 
